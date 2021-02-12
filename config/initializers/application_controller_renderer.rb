4b107d2fdfd4701f8c2580bec5678ff8151986da
Ryo-2525 <seama.24.music@gmail.com>
2021-02-12 04:53:26 +0000

    [start]upload

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'be9aede48b264425ae094d827800edb716b9a35e5b3605f5ba31fc8381faa41852c796c1cfbea2dd9a7d86d2ddcb3348017b258c44624e33a574e84c0e03e7ac'
